f4b279117fac3dae55b5c6764f0f3791dcf7ab99
DucAnh21092000 <dinhanh21092000@gmail.com>
2022-12-31 17:37:03 +0700

    done

---
+++ js/animation.js
@@ -0,0 +1,98 @@
+//          Tác giả : Đinh Đức Anh
+//          Mail    : anh.dd182330@sis.hust.edu.vn
+//                    dinhanh21092000@gmail.com
+
+// Object chứa giá trị cần hiển thị
+let initialValue = {
+    year: "2023",
+    content: "Happy New Year !!",
+    icon: " ❤❤❤❤",
+    mess: "Chúc mừng năm mới bé yêu ! /" +
+        "Chúc bé năm mới vui vẻ, luôn mỉm cười, gặp nhiêu may mắn trong cuộc sống,"
+        + "/ Yêu anh nhiều hơn. Anh yêu em  ❤️ "
+
+}
+
+const year = document.querySelector('.year')
+const content = document.querySelector('.content')
+const icon = document.querySelector('.icon')
+const mess = document.querySelector('.mess')
+
+let arrYear = initialValue.year.split('')
+let arrContent = (initialValue.content.split(""))
+let arrIcon = initialValue.icon.split('')
+let arrMess = initialValue.mess.split('')
+
+let arrYearResult = []
+let arrContentResult = []
+let arrIconResult = []
+let arrMessResult = []
+
+// Hiển thị tin nhắn muốn hiển thị
+const displayMess = () => {
+    setTimeout(() => {
+        const timeOut = setInterval(() => {
+            let arrShift = arrMess.shift()
+            if (arrShift == '/') {
+                arrMessResult.push('<br>')
+            }
+            else {
+                arrMessResult.push(arrShift)
+            }
+            mess.innerHTML = arrMessResult.join("")
+            if (arrMess.length == 0) {
+                clearInterval(timeOut)
+                displayIcon()
+            }
+        }, 300) // thời gian lấy ra từng chữ 1 và hiển thị
+    }, 1000) // thời gian delay
+}
+
+// Hiển thị nội dung lời chúc
+const displayContent = () => {
+    setTimeout(() => {
+        const timeOut = setInterval(() => {
+            arrContentResult.push(arrContent.shift())
+            content.innerHTML = arrContentResult.join("")
+
+            if (arrContent.length == 0) {
+                clearInterval(timeOut)
+                displayMess()
+            }
+        }, 500)
+    }, 1000)
+}
+
+// hiển thị icon
+const displayIcon = () => {
+    setTimeout(() => {
+        const timeOut = setInterval(() => {
+            arrIconResult.push(arrIcon.shift())
+            icon.innerHTML = arrIconResult.join("")
+
+            if (arrIcon.length == 0) {
+                clearInterval(timeOut)
+
+            }
+        }, 100)
+    }, 600)
+}
+
+// function chạy đầu tiên
+function display() {
+    setTimeout(() => {
+        const timeOut = setInterval(() => {
+            arrYearResult.push(arrYear.shift())
+            year.classList.add('.animation')
+            year.innerHTML = arrYearResult.join("")
+            year.classList.remove('.animation')
+            if (arrYear.length == 0) {
+                clearInterval(timeOut)
+                displayContent()
+            }
+        }, 1000)
+    }, 2000)
+
+
+}
+display()
